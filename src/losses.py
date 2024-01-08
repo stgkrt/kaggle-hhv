e405c8423a820a600c2106dae7fb750ebe428c5e
stgkrt <taro.stst@gmail.com>
2024-01-08 04:11:11 +0000

    :sparkles: dice loss

---
+++ src/losses.py
@@ -0,0 +1,19 @@
+import torch.nn as nn
+
+
+class DiceLoss(nn.Module):
+    def __init__(self):
+        super(DiceLoss, self).__init__()
+
+    def forward(self, inputs, targets, smooth=1):
+        # comment out if your model contains a sigmoid or equivalent activation layer
+        inputs = inputs.sigmoid()
+
+        # flatten label and prediction tensors
+        inputs = inputs.view(-1)
+        targets = targets.view(-1)
+
+        intersection = (inputs * targets).sum()
+        dice = (2.0 * intersection + smooth) / (inputs.sum() + targets.sum() + smooth)
+
+        return 1 - dice
